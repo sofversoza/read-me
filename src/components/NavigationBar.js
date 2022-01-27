19ee18ac4039c9510addf4eb18b203756fd1c098
Grant Pierce <glpierce174@gmail.com>
2022-01-27 09:38:01 -0800

    Fixed navbar Links

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, NavDropdown, Container, Form, FormControl, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import AccountDropdown from './AccountDropdown'
 
 function NavigationBar({ setCategoryViewParams, setSearchValues, setSearchResults }) {
@@ -13,19 +14,19 @@ function NavigationBar({ setCategoryViewParams, setSearchValues, setSearchResult
   return (
       <Nav style={{ backgroundColor:'#DCC2AE'}} fill variant="tabs">
         <Nav.Item>
-          <Nav.Link to="/" onClick={e => categoryDisplay("Best Sellers")} eventKey="1" style={{color:'black'}}>Best Sellers</Nav.Link>
+          <Link to="/" onClick={e => categoryDisplay("Best Sellers")} eventKey="1" style={{color:'black', textDecoration: 'none'}}>Best Sellers</Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link to="/" onClick={e => categoryDisplay("Highest Rated")} eventKey="2" style={{color:'black'}}>Highest Rated</Nav.Link>
+          <Link to="/" onClick={e => categoryDisplay("Highest Rated")} eventKey="2" style={{color:'black', textDecoration: 'none'}}>Highest Rated</Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link to="/" onClick={e => categoryDisplay("New Releases")} eventKey="3" style={{color:'black'}}>New Releases</Nav.Link>
+          <Link to="/" onClick={e => categoryDisplay("New Releases")} eventKey="3" style={{color:'black', textDecoration: 'none'}}>New Releases</Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link to="/" onClick={e => categoryDisplay("Deals")} eventKey="4" style={{color:'black'}}>Deals</Nav.Link>
+          <Link to="/" onClick={e => categoryDisplay("Deals")} eventKey="4" style={{color:'black', textDecoration: 'none'}}>Deals</Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link to="/cart" eventKey="5" style={{color:'black'}}>Cart</Nav.Link>
+          <Link to="/cart" eventKey="5" style={{color:'black', textDecoration: 'none'}}>Cart</Link>
         </Nav.Item>
         <Nav.Item>
           <AccountDropdown />
